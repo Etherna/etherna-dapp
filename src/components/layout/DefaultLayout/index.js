9c23b547cefd3837262b26f8a781a973aaa23f87
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-02-16 18:40:57 +0100

    Renamed components to uppercase

--- src/components/layout/DefaultLayout/index.js
+++ src/components/layout/DefaultLayout/index.js
@@ -9,8 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "../header"
-import Sidebar from "../sidebar"
+import Header from "../Header"
+import Sidebar from "../Sidebar"
 import "./layout.scss"
 
 const Layout = ({ children }) => {
