20c219281d48e744c47633373f7c1b6d90939f2e
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-02-17 09:24:49 +0100

    Added modal component

--- tailwind.config.js
+++ tailwind.config.js
@@ -353,7 +353,16 @@ module.exports = {
         },
         minWidth: {
             "0": "0",
-            "48": "12rem",
+            xs: "20rem",
+            sm: "24rem",
+            md: "28rem",
+            lg: "32rem",
+            xl: "36rem",
+            "2xl": "42rem",
+            "3xl": "48rem",
+            "4xl": "56rem",
+            "5xl": "64rem",
+            "6xl": "72rem",
             full: "100%",
         },
         objectPosition: {
