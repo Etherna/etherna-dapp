59c3d5d4bc490f515c18c268cf24990882be2a0b
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-03-20 10:58:09 +0100

    Fixed purgecss config

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
                 printRejected: true, // Print removed selectors and processed file names
                 tailwind: true, // Enable tailwindcss support
                 // whitelist: ['whitelist'], // Don't remove this selector
-                // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+                ignore: ['src/components'], // Ignore files/folders
                 // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
             },
         },
