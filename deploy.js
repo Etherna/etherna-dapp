5a071903ab0b95e2ab227b4d82af25cbda42a31f
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-04-05 16:44:45 +0200

    Updated gitignore

---
+++ deploy.js
@@ -0,0 +1,51 @@
+const deployToHost = () => {
+    const FtpDeploy = require("ftp-deploy")
+    const config = require("./deploy.config")
+    const moment = require("moment")
+
+    const start = moment()
+    const ftpDeploy = new FtpDeploy()
+    ftpDeploy.on("uploading", function(data) {
+        console.log(data.transferredFileCount + "/" + data.totalFilesCount + " - " + data.filename)
+    })
+    ftpDeploy.on("upload-error", function(data) {
+        console.log(data.err)
+    })
+
+    ftpDeploy
+        .deploy(config)
+        .then(data => {
+            const end = moment()
+            const timespan = end.diff(start, "seconds")
+            const minutes = Math.floor(timespan / 60)
+            const seconds = timespan - minutes * 60
+            console.log(`Deploy completed in ${minutes}m and ${seconds}s`)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+const deployToSwarm = async () => {
+    // TOOD
+    const axios = require("axios")
+    try {
+        const resp = await axios.post("https://gateway.etherna.io/bzz:/", "hello")
+        console.log(resp.data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const deploy = () => {
+    const args = process.argv
+    const isSwarm = args.length > 2 && args[2] === "--swarm"
+
+    if (isSwarm) {
+        deployToSwarm()
+    } else {
+        deployToHost()
+    }
+}
+
+deploy()
