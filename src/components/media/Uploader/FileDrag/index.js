04f121d84605e18d45175d43b01afb58074a4cc9
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-04-09 17:47:42 +0200

    Added upload limit

--- src/components/media/Uploader/FileDrag/index.js
+++ src/components/media/Uploader/FileDrag/index.js
@@ -3,9 +3,11 @@ import PropTypes from "prop-types"
 import classnames from "classnames"
 
 import "./file-drag.scss"
+import Alert from "@components/common/Alert"
 
-const FileDrag = ({ id, label, onSelectFile, disabled }) => {
+const FileDrag = ({ id, label, onSelectFile, disabled, uploadLimit }) => {
     const [isDragOver, setIsDragOver] = useState(false)
+    const [showSizeLimitError, setShowSizeLimitError] = useState(false)
 
     const updateDragOver = hasEntered => {
         if ((hasEntered && !isDragOver) || (!hasEntered && isDragOver)) {
@@ -38,45 +40,69 @@ const FileDrag = ({ id, label, onSelectFile, disabled }) => {
 
         const files = [...e.dataTransfer.files]
 
+        handleFileSelect(files)
+    }
+
+    const handleFileSelect = files => {
         if (files && files.length > 0) {
-            onSelectFile(files[0])
+            if (!uploadLimit || files[0].size <= uploadLimit * 1024 * 1024) {
+                onSelectFile(files[0])
+            } else {
+                setShowSizeLimitError(true)
+            }
         }
     }
 
     return (
-        <div
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-            onDragEnter={handleDragEnter}
-            onDragLeave={handleDragLeave}
-            role="button"
-            tabIndex={0}
-        >
-            <label
-                htmlFor={id}
-                className={classnames("drag-input", {
-                    "drag-over": isDragOver,
-                })}
-            >
-                <input
-                    type="file"
-                    id={id}
-                    onChange={e => onSelectFile(e.target.files[0])}
-                    disabled={disabled}
-                />
-                <div className="drag-content">
-                    <img
-                        src={require("@svg/icons/upload-icon-lg.svg")}
-                        alt=""
-                    />
-                    <span className="drag-info text-lg">
-                        {label || "Drag here"}
-                    </span>
-                    <span className="drag-info text-sm font-normal">or</span>
-                    <div className="btn btn-outline">Select</div>
+        <>
+            {showSizeLimitError && (
+                <div className="mb-2">
+                    <Alert
+                        title="File size exceeded"
+                        type="warning"
+                        onClose={() => setShowSizeLimitError(false)}
+                    >
+                        Your file is too large. The maximum upload size is currently {uploadLimit}MB.
+                    </Alert>
                 </div>
-            </label>
-        </div>
+            )}
+            <div
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
+                onDragEnter={handleDragEnter}
+                onDragLeave={handleDragLeave}
+                role="button"
+                tabIndex={0}
+            >
+                <label
+                    htmlFor={id}
+                    className={classnames("drag-input", {
+                        "drag-over": isDragOver,
+                    })}
+                >
+                    <input
+                        type="file"
+                        id={id}
+                        onChange={e => handleFileSelect(e.target.files)}
+                        disabled={disabled}
+                    />
+                    <div className="drag-content">
+                        <img
+                            src={require("@svg/icons/upload-icon-lg.svg")}
+                            alt=""
+                        />
+                        <span className="drag-info text-lg">
+                            {label || "Drag here"}
+                        </span>
+                        <span className="drag-info text-sm font-normal">or</span>
+                        <div className="btn btn-outline">Select</div>
+                        {uploadLimit && (
+                            <small className="bold">{uploadLimit}MB</small>
+                        )}
+                    </div>
+                </label>
+            </div>
+        </>
     )
 }
 
@@ -85,6 +111,7 @@ FileDrag.propTypes = {
     label: PropTypes.string,
     onSelectFile: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
+    uploadLimit: PropTypes.number,
 }
 
 export default FileDrag
