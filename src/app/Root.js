dbb1ec98582dc4b0b73739fce8888ea85d3d7793
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-06-07 16:00:52 +0200

    Fixed chunks

--- src/app/Root.js
+++ src/app/Root.js
@@ -14,116 +14,46 @@ import PageLoader from "@common/PageLoader"
 import Layout from "@components/layout/DefaultLayout"
 import { ShortcutWrapper } from "@keyboard"
 import StateWrapper from "@state/wrapper"
-import { getBasename, isMatch } from "@routes"
+import { getBasename } from "@routes"
 
-const AsyncHome = loadable(
-    () => pMinDelay(import(/* webpackChunkName: "home" */ "@pages/home"), 200),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncChannel = loadable(
-    () =>
-        pMinDelay(
-            import(/* webpackChunkName: "channel" */ "@pages/channel"),
-            200
-        ),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncChannelEdit = loadable(
-    () =>
-        pMinDelay(
-            import(/* webpackChunkName: "channel-edit" */ "@pages/channelEdit"),
-            200
-        ),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncChannels = loadable(
-    () =>
-        pMinDelay(
-            import(/* webpackChunkName: "channels" */ "@pages/channels"),
-            200
-        ),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncWatch = loadable(
-    () =>
-        pMinDelay(import(/* webpackChunkName: "watch" */ "@pages/watch"), 200),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncUpload = loadable(
-    () =>
-        pMinDelay(
-            import(/* webpackChunkName: "upload" */ "@pages/upload"),
-            200
-        ),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncHowItWorks = loadable(
-    () =>
-        pMinDelay(
-            import(
-                /* webpackChunkName: "how-it-works" */ "@pages/how-it-works"
-            ),
-            200
-        ),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncShortcuts = loadable(
-    () =>
-        pMinDelay(
-            import(/* webpackChunkName: "shortcuts" */ "@pages/shortcuts"),
-            200
-        ),
-    {
-        fallback: <PageLoader />,
-    }
-)
-const AsyncNotFound = loadable(
-    () => pMinDelay(import(/* webpackChunkName: "404" */ "@pages/404"), 200),
-    {
-        fallback: <PageLoader />,
-    }
-)
+const AsyncHome = loadable(() => pMinDelay(import(/* webpackChunkName: "home" */ "@pages/home"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncChannel = loadable(() => pMinDelay(import(/* webpackChunkName: "channel" */ "@pages/channel"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncChannelEdit = loadable(() => pMinDelay(import(/* webpackChunkName: "channel-edit" */ "@pages/channelEdit"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncChannels = loadable(() => pMinDelay(import(/* webpackChunkName: "channels" */ "@pages/channels"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncWatch = loadable(() => pMinDelay(import(/* webpackChunkName: "watch" */ "@pages/watch"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncUpload = loadable(() => pMinDelay(import(/* webpackChunkName: "upload" */ "@pages/upload"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncHowItWorks = loadable(() => pMinDelay(import(/* webpackChunkName: "how-it-works" */ "@pages/how-it-works"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncShortcuts = loadable(() => pMinDelay(import(/* webpackChunkName: "shortcuts" */ "@pages/shortcuts"), 200), {
+    fallback: <PageLoader />
+})
+const AsyncNotFound = loadable(() => pMinDelay(import(/* webpackChunkName: "404" */ "@pages/404"), 200), {
+    fallback: <PageLoader />
+})
 
 const basename = getBasename()
 
-const Home = isMatch("/", true)
-    ? require("@pages/home").default
-    : () => <AsyncHome />
-const Channel = isMatch("/channel/:id", true)
-    ? require("@pages/channel").default
-    : () => <AsyncChannel />
-const ChannelEdit = isMatch("/channel/:id/edit", true)
-    ? require("@pages/channelEdit").default
-    : () => <AsyncChannelEdit />
-const Channels = isMatch("/channels", true)
-    ? require("@pages/channels").default
-    : () => <AsyncChannels />
-const Watch = isMatch("/watch", false)
-    ? require("@pages/watch").default
-    : () => <AsyncWatch />
-const Upload = isMatch("/upload", false)
-    ? require("@pages/upload").default
-    : () => <AsyncUpload />
-const HowItWorks = isMatch("/how-it-works", false)
-    ? require("@pages/how-it-works").default
-    : () => <AsyncHowItWorks />
-const Shortcuts = isMatch("/shortcuts", false)
-    ? require("@pages/shortcuts").default
-    : () => <AsyncShortcuts />
+const Home = () => <AsyncHome />
+const Channel = () => <AsyncChannel />
+const ChannelEdit = () => <AsyncChannelEdit />
+const Channels = () => <AsyncChannels />
+const Watch = () => <AsyncWatch />
+const Upload = () => <AsyncUpload />
+const HowItWorks = () => <AsyncHowItWorks />
+const Shortcuts = () => <AsyncShortcuts />
 const NotFound = () => <AsyncNotFound />
 
 const Root = () => {
