19c99f6e66f96f59e00df98f8b21e333d23cbf65
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-06-16 16:20:49 +0200

    Implemented http deploy.

--- deploy.swarm.js
+++ deploy.swarm.js
@@ -1,32 +1,61 @@
+const fs = require("fs")
+const glob = require("glob")
+const mimetypes = require("mime-types")
 const config = require("./deploy.config")
+const { BzzNode } = require("@erebos/swarm-node")
 
-const execCommand = cmd => {
-    const { exec } = require("child_process")
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                reject(error)
-            } else if (stderr) {
-                reject(stderr)
-            } else {
-                resolve(stdout)
+/**
+ * @returns {string[][]}
+ */
+const readFiles = () => new Promise((resolve, reject) => {
+    glob(config.localRoot + "/**/*", (error, result) => {
+        if (error) reject (error)
+
+        let files = []
+        result.forEach(path => {
+            if (fs.lstatSync(path).isFile()) {
+                let file = path.replace(config.localRoot, "")
+                if (file.startsWith("/")) file = file.substr(1, file.length - 1)
+                files.push([file, path])
             }
         })
+
+        resolve(files)
     })
-}
+})
 
 const deployToSwarm = async () => {
     try {
         // Upload build folder to swarm
         console.log("Uploading build folder to swarm...")
-        const command = `swarm --bzzapi ${config.swarmGateway} --defaultpath build/index.html --recursive up build`
-        const manifest = await execCommand(command)
-        const hash = "0x" + manifest
+
+        const files = await readFiles()
+
+        // Creating form data
+        let data = {
+            // default path
+            "": {
+                data: fs.readFileSync(`${config.localRoot}/index.html`),
+                contentType: "text/html",
+            },
+        }
+
+        files.forEach(fileInfo => {
+            const [fileName, filePath] = fileInfo
+            data[fileName] = {
+                data: fs.readFileSync(filePath),
+                contentType: mimetypes.lookup(filePath),
+            }
+        })
+
+        // Upload
+        const bzz = new BzzNode({ url: config.swarmGateway })
+        const manifest = await bzz.uploadDirectory(data)
 
         console.log(`App deployed to ${manifest}`)
 
         // return hash for the feed
-        return hash
+        return `0x${manifest}`
     } catch (error) {
         console.error(error)
     }
