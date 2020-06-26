3c63d9accd29a6e4c07ca07d82901d0a4199c9fd
Mattia Dalzocchio <mattiadalzocchio@me.com>
2020-06-26 08:51:07 +0200

    Added footer links with feedback.

--- src/components/layout/Sidebar/Sidebar.js
+++ src/components/layout/Sidebar/Sidebar.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 import "./sidebar.scss"
 import MyChannel from "./MyChannel"
 import NewChannels from "./NewChannels"
+import Routes from "@routes"
 
 const Sidebar = () => {
     return (
@@ -12,7 +14,18 @@ const Sidebar = () => {
                 <NewChannels />
             </div>
             <small className="footer-notice">
-                Copyright © {new Date().getFullYear()} etherna.io.
+                <nav className="footer-menu">
+                    <Link to={Routes.getHowItWorksLink()} className="footer-link">
+                        How it works
+                    </Link>
+                    <button id="jira_feedback_btn" className="footer-link">
+                        Feedback
+                    </button>
+                    <a href="https://index.etherna.io/swagger" className="footer-link" target="_blank" rel="noopener noreferrer">
+                        Index Api
+                    </a>
+                </nav>
+                Copyright © {new Date().getFullYear()} Etherna Sagl.
             </small>
         </aside>
     )
