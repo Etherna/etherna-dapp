defcb19091db96809f2a82f6f5421eb5b6e57aa6
Mattia Dalzocchio <mattiadalzocchio@me.com>
2021-05-13 09:35:39 +0200

    Replace react-rte with draftjs

--- src/components/common/MarkdownEditor/MarkdownEditor.tsx
+++ src/components/common/MarkdownEditor/MarkdownEditor.tsx
@@ -1,81 +1,123 @@
 import React, { useEffect, useState } from "react"
-import Editor, { EditorValue, ToolbarConfig } from "react-rte"
+import classnames from "classnames"
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  DraftHandleValue,
+  DraftEditorCommand,
+  convertFromRaw,
+  convertToRaw
+} from "draft-js"
+import { draftToMarkdown, markdownToDraft } from "markdown-draft-js"
 
 import "./markdown-editor.scss"
+import MarkdownEditorButton, { MarkdownButtonConfig } from "./MarkdownEditorButton"
 
 type MarkdownEditorProps = {
   value: string
   placeholder?: string
   disabled?: boolean
-  onChange?: (markdown: string) => void
+  onChange?(markdown: string): void
 }
 
-const MarkdownEditor = ({
+type ToolbarConfig = {
+  [key: string]: MarkdownButtonConfig[]
+}
+
+const toolbarConfig: ToolbarConfig = {
+  INLINE_STYLE_BUTTONS: [
+    { label: "Bold", type: "inline", style: "BOLD" },
+    { label: "Italic", type: "inline", style: "ITALIC" },
+    { label: "Underline", type: "inline", style: "UNDERLINE" },
+    { label: "Strikethrough", type: "inline", style: "STRIKETHROUGH" },
+    { label: "Code", type: "inline", style: "CODE" },
+  ],
+  BLOCK_ALIGNMENT_BUTTONS: [
+    { label: "Align Left", type: "block", style: "ALIGN_LEFT" },
+    { label: "Align Center", type: "block", style: "ALIGN_CENTER" },
+    { label: "Align Right", type: "block", style: "ALIGN_RIGHT" },
+    { label: "Align Justify", type: "block", style: "ALIGN_JUSTIFY" },
+  ],
+  BLOCK_TYPE_BUTTONS: [
+    { label: "Unordered list", type: "block", style: "unordered-list-item" },
+    { label: "Ordered list", type: "block", style: "ordered-list-item" },
+    { label: "Code block", type: "block", style: "code-block" }
+  ]
+}
+
+const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
   value,
   placeholder,
   disabled,
   onChange,
-}: MarkdownEditorProps) => {
-  const [state, setState] = useState(Editor.createValueFromString(value, "markdown"))
+}) => {
+  const [markdown, setMarkdown] = useState(value)
+  const [state, setState] = useState<EditorState>(() => {
+    const rawData = markdownToDraft(value)
+    const contentState = convertFromRaw(rawData)
+    const newEditorState = EditorState.createWithContent(contentState)
+    return newEditorState
+  })
+  const [hasFocus, setHasFocus] = useState(false)
 
   useEffect(() => {
     if (value === "") {
-      setState(Editor.createValueFromString(value, "markdown"))
+      // reset state
+      setState(EditorState.createEmpty())
+      setMarkdown(value)
     }
   }, [value])
 
-  const handleChange = (newValue?: EditorValue) => {
-    if (!newValue) return
+  const handleChange = (editorState: EditorState) => {
+    setState(editorState)
 
-    setState(newValue)
+    const content = editorState.getCurrentContent()
+    const rawObject = convertToRaw(content)
+    const markdownString = draftToMarkdown(rawObject)
 
-    if (onChange) {
-      onChange(newValue.toString("markdown"))
+    if (markdownString !== markdown) {
+      setMarkdown(markdownString)
+      onChange?.(markdownString)
     }
   }
 
-  const toolbarConfig: ToolbarConfig = {
-    // Optionally specify the groups to display (displayed in the order listed).
-    display: [
-      "INLINE_STYLE_BUTTONS",
-      "BLOCK_ALIGNMENT_BUTTONS",
-      "BLOCK_TYPE_BUTTONS",
-      "LINK_BUTTONS",
-      "BLOCK_TYPE_DROPDOWN",
-      "HISTORY_BUTTONS"
-    ],
-    INLINE_STYLE_BUTTONS: [
-      { label: "Bold", style: "BOLD" },
-      { label: "Italic", style: "ITALIC" },
-      { label: "Strikethrough", style: "STRIKETHROUGH" },
-      { label: "Code", style: "CODE" },
-      { label: "Underline", style: "UNDERLINE" },
-    ],
-    BLOCK_ALIGNMENT_BUTTONS: [
-      { label: "Align Left", style: "ALIGN_LEFT" },
-      { label: "Align Center", style: "ALIGN_CENTER" },
-      { label: "Align Right", style: "ALIGN_RIGHT" },
-      { label: "Align Justify", style: "ALIGN_JUSTIFY" },
-    ],
-    BLOCK_TYPE_DROPDOWN: [
-      { label: "Normal", style: "unstyled" },
-      { label: "Code Block", style: "code-block" },
-    ],
-    BLOCK_TYPE_BUTTONS: [
-      { label: "UL", style: "unordered-list-item" },
-      { label: "OL", style: "ordered-list-item" }
-    ]
+  const handleKeyCommand = (command: DraftEditorCommand, editorState: EditorState): DraftHandleValue => {
+    const newState = RichUtils.handleKeyCommand(editorState, command)
+
+    if (newState) {
+      setState(newState)
+      return "handled"
+    }
+
+    return "not-handled"
   }
 
   return (
-    <div className="markdown-editor">
+    <div className={classnames("markdown-editor", { focused: hasFocus })}>
+      <div className="markdown-editor-toolbar">
+        {Object.keys(toolbarConfig).map(key => (
+          <div className="markdown-editor-btn-group" key={key}>
+            {toolbarConfig[key].map((btnConfig, i) => (
+              <MarkdownEditorButton
+                editorState={state}
+                config={btnConfig}
+                onEditorStateChange={handleChange}
+                key={i}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
       <Editor
         placeholder={placeholder}
-        toolbarConfig={toolbarConfig}
-
-        value={state}
+        editorState={state}
+        handleKeyCommand={handleKeyCommand}
         onChange={handleChange}
-        disabled={disabled}
+        onFocus={() => setHasFocus(true)}
+        onBlur={() => setHasFocus(false)}
+        readOnly={disabled}
+        spellCheck
       />
     </div>
   )
