eb37768039d297f90cd6ccf0139a188330cf398a
Mattia Dalzocchio <mattiadalzocchio@me.com>
2021-10-22 14:10:10 +0200

    Enable prefetch data for only crawler bots

--- src/prefetch/index.ts
+++ src/prefetch/index.ts
@@ -1,12 +1,12 @@
-/* 
+/*
  *  Copyright 2021-present Etherna Sagl
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 import profilePrefetcher from "./prefetchers/profilePrefetcher"
 import videoPrefetcher from "./prefetchers/videoPrefetcher"
+import { isBotUserAgent } from "@utils/browser"
 
 const prefetchers = [
   profilePrefetcher,
@@ -25,10 +26,12 @@ const prefetchers = [
 const prefetch = async (renderCallback: () => void) => {
   window.prefetchData = undefined
 
-  for (const prefetcher of prefetchers) {
-    if (prefetcher.match.test(window.location.pathname)) {
-      await prefetcher.fetch()
-      break
+  if (isBotUserAgent()) {
+    for (const prefetcher of prefetchers) {
+      if (prefetcher.match.test(window.location.pathname)) {
+        await prefetcher.fetch()
+        break
+      }
     }
   }
 
--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,12 +1,12 @@
-/* 
+/*
  *  Copyright 2021-present Etherna Sagl
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,4 +59,11 @@ export const isTouchDevice = () => {
   return (("ontouchstart" in window) ||
     (navigator.maxTouchPoints > 0) ||
     (navigator.msMaxTouchPoints > 0))
+}
+
+/**
+ * Check if current user agent is a bot or crawler
+ */
+export const isBotUserAgent = () => {
+  return /bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)
 }
