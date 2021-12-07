30864d3d5fa0834a80f93ba24261706a592e9617
Mattia Dalzocchio <mattiadalzocchio@me.com>
2021-12-07 10:48:42 +0100

    Fix context logger

--- src/utils/context-logger.ts
+++ src/utils/context-logger.ts
@@ -1,12 +1,12 @@
-/* 
+/*
  *  Copyright 2021-present Etherna Sagl
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,19 +25,27 @@ const getCurrentTimeFormatted = () => {
 
 type Reducer<State = any, Action = any> = (state: State, action: Action) => State
 
+let lastAction: any
+
 const logger = <R extends Reducer>(reducer: R): R => {
   const reducerWithLogger = (state: any, action: any) => {
     const next = reducer(state, action)
-    console.group(
-      `%caction: %c${action.type} %cat ${getCurrentTimeFormatted()}`,
-      "color: lightgray; font-weight: 300;",
-      "color: white; font-weight: bold;",
-      "color: lightblue; font-weight: lighter;"
-    )
-    console.log("%cprev state:", "color: #9E9E9E; font-weight: 700;", state)
-    console.log("%caction:", "color: #00A7F7; font-weight: 700;", action)
-    console.log("%cnext state:", "color: #47B04B; font-weight: 700;", next)
-    console.groupEnd()
+
+    if (action !== lastAction) {
+      console.group(
+        `%caction: %c${action.type} %cat ${getCurrentTimeFormatted()}`,
+        "color: lightgray; font-weight: 300;",
+        "color: white; font-weight: bold;",
+        "color: lightblue; font-weight: lighter;"
+      )
+      console.log("%cprev state:", "color: #9E9E9E; font-weight: 700;", state)
+      console.log("%caction:", "color: #00A7F7; font-weight: 700;", action)
+      console.log("%cnext state:", "color: #47B04B; font-weight: 700;", next)
+      console.groupEnd()
+    }
+
+    lastAction = action
+
     return next
   }
 
