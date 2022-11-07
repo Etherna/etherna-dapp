237be2199a44e3fe4667efe69ba9d180aea3bf28
Mattia Dalzocchio <mattiadalzocchio@me.com>
2022-11-07 13:58:03 +0100

    fix build errors

--- scripts/bee-https-proxy.mjs
+++ scripts/bee-https-proxy.mjs
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 import chalk from "chalk"
 import fs from "fs"
 import https from "https"
